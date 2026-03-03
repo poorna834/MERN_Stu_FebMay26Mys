aa7e2de51f62a93798e4d56cb11d8d1ae9525b96
poorna834 <poornashree.lm@gmail.com>
2026-03-03 15:12:13 +0530

    Arithematic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,15 @@
+// Arithematic Operators
+console.log("Arithematic Operators");
+const a = 2, b = 3;
+let c = 4;
+console.log("a+b: ",c=a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b);
+console.log("a**b: ",a**b);
+console.log("a++: ",a++);
+console.log("++a: ",++a);
+console.log("a--: ",a--);
+console.log("--a: ",--a);
+ 
