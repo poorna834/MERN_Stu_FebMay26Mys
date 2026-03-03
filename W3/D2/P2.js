d8d45124d6ca08d81e7344ac1af4b0f153c66084
poorna834 <poornashree.lm@gmail.com>
2026-03-03 15:12:54 +0530

    Comparison  Operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+// Comparison operators
+
+console.log("Comparison Operators");
+let x=5 , y='6';
+console.log("x==y: ",x==y);  //Loose Equality
+console.log("x===y: ",x===y);  //Strict Equality
+console.log("x!=y: ",x!=y);  //Loose InEquality
+console.log("x!==y: ",x!==y);  //Strict InEquality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=3: ",x<=3);
