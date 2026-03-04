d0640bcefc3fdbd57dea1ab145f3257195c93e67
poorna834 <poornashree.lm@gmail.com>
2026-03-04 14:26:12 +0530

    More on basics of objects

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -10,3 +10,22 @@ console.log("Person",person);
 console.log("Name",person.name);
 
 console.log("Age",person["age"]);
+
+// add a new property
+person.city="Mysore";
+console.log("Person",person);
+
+// Modify
+person.age=31;
+console.log("Person",person);
+
+// delete
+delete person.isStudent;
+console.log("Person",person);
+
+// Object Constructor
+const car = new Object();
+car.make = "Audi";
+car.model = "A4";
+car.year = 2026;
+console.log("CAR",car);
