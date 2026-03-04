cdc3d5b03c4eda5d21d41f2005f8b010525d9613
poorna834 <poornashree.lm@gmail.com>
2026-03-04 12:19:17 +0530

    Break & Continue Statements

---
+++ W3/D3/P3.js
@@ -0,0 +1,16 @@
+// Break & Continue Statement
+
+for(let i=0;i<5;i++){   
+    if(i===2){
+    console.log("i=",i);
+    break;
+    }
+}
+
+for(let i=0;i<5;i++){  
+    if(i===2){
+    console.log("i=",i);
+    continue;
+    }
+    console.log("i=",i);
+}
