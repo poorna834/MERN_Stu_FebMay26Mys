d78f079486035d89171e2305b15ee4cbb094a672
poorna834 <poornashree.lm@gmail.com>
2026-03-04 13:28:46 +0530

    Basics of objects

---
+++ W3/D3/P5.js
@@ -0,0 +1,12 @@
+// Basics of objects
+
+const person = {
+    name:"Rahul",
+    age:30,
+    isStudent: false
+};
+console.log("Person",person);
+
+console.log("Name",person.name);
+
+console.log("Age",person["age"]);
