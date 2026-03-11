0ff2aad9393ec2cdabd1eb284a0bfd2698657666
poorna834 <poornashree.lm@gmail.com>
2026-03-11 12:33:11 +0530

    Saving And Retreiving JSON Data JS

---
+++ W4/D3/P4.js
@@ -0,0 +1,19 @@
+const jsonOutput = document.getElementById("jsonOutput");
+
+document.getElementById("saveBtn").addEventListener("click",function(){
+    const user = {
+        id : 101,
+        name : "Shamanth",
+        role : "Full Stack Developer",
+        skills : ["HTML","CSS","JS"]
+    };
+    localStorage.setItem("userProfile",JSON.stringify(user));
+    jsonOutput.textContent ="user object saved as string to localStorage";
+});
+
+document.getElementById("readBtn").addEventListener("click",function(){
+    const up = localStorage.getItem("userProfile");
+    console.log(JSON.parse(up))
+    console.log(up);
+    jsonOutput.textContent = "userProfile" + up;
+});
