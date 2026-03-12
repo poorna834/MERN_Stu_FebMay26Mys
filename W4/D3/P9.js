4b9ef40d5c86ebfeaf3938f34d7a8b57bb0c3f07
poorna834 <poornashree.lm@gmail.com>
2026-03-12 11:30:15 +0530

    Async Fetch With Input Validation JS

---
+++ W4/D3/P9.js
@@ -0,0 +1,26 @@
+// async Fetch with input and validation
+const asyncFetchBtn = document.getElementById("asyncFetchBtn");
+const output = document.getElementById("output");
+const postIdInput = document.getElementById("postIdInput");
+
+asyncFetchBtn.addEventListener("click", async function () { 
+        const id = postIdInput.value.trim();
+        if (id === "") {
+            output.textContent = "Post ID is required.";
+            return;
+        }
+        const numericId = Number(id);
+        if (numericId < 1 || numericId > 100) {
+            output.textContent = "Number should be between 1 to 100";
+            return;
+        }
+    try {
+        output.textContent = "Fetching Post..."
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/" + numericId);
+        if (!response.ok) throw new Error("HTTP error: " + response.status);
+        const data = await response.json();
+        output.textContent = JSON.stringify(data, null, 2);
+    } catch (error) {
+        output.textContent = "Error: " + error.message;
+    }
+});
