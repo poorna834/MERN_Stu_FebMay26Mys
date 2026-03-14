8e07a0c6ba48fec15df8d87201bddd231ca735eb
poorna834 <poornashree.lm@gmail.com>
2026-03-14 16:38:08 +0530

    Portfolio Project

---
+++ W4/D5/Portfolio Project/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+    // Render Skills
+    renderSkills();
+    initModal();
+    initContactValidation();
+    initThemeToggle();
+    renderProjects();
+})
+
