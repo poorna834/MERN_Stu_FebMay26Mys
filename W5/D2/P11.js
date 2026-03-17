3dd6b240fe6f28778873a7df366945c307385793
poorna834 <poornashree.lm@gmail.com>
2026-03-17 15:38:26 +0530

    Chaining Promises With Returned Promises

---
+++ W5/D2/P11.js
@@ -0,0 +1,31 @@
+// Chaining Promises With Returned Promises
+
+function getOrderId(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(501);
+        },500);
+    });
+}
+
+function getOrderDetails(orderId){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve({
+                id:orderId,
+                product:"Laptop",
+                quantity:2
+            });
+        },700);
+    });
+}
+getOrderId()
+.then(function(orderId){
+    console.log("Order Id Received: ",orderId);
+    return getOrderDetails(orderId);
+})
+.then(function(OrderDetails){
+    console.log("Order details loaded.");
+    console.log("Product: ",OrderDetails.product);
+    console.log("Quantity: ",OrderDetails.quantity);
+});
