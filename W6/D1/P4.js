9f91611a0c50f22b39d669c8fce1e4992a0f16c1
poorna834 <poornashree.lm@gmail.com>
2026-03-23 12:30:39 +0530

    Usage Of Built-In And Third-Party Modules.

--- W6/D1/P4.js
+++ W6/D1/P4.js
@@ -5,3 +5,12 @@ const path =  require("path");
 
 const invoicePath = path.join("invoices","2026","invoice_001.txt")
 console.log("Built-In Module Result: ",invoicePath);
+
+// Usage Third Party Package/Module
+try{
+    const_=require("lodash");
+    console.log("Third-Party Modules Example");
+}
+catch(error){
+    console.log("Third Party Module 'lodash' is not installed.")
+}
