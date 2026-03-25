1f53996fe1bbaf18d6c5f20cc356168b97b4b162
poorna834 <poornashree.lm@gmail.com>
2026-03-25 10:41:36 +0530

    Reading & writing files synchronously

--- W6/D3/P2.js
+++ W6/D3/P2.js
@@ -1,28 +1,24 @@
-// Reading and writing file synchronously
+//Reading & writing files synchronously
 
 const fs = require("fs");
 const path = require("path");
 
 const filePath = path.join(__dirname,"sync-note.txt");
 
-// Syntax for function usage in a module
-// moduleName.functionName()
+// fs.writeFileSync(filePath,"This file was written using writeFileSync().\nSynchronous operation block execution.");
 
-// fs.writeFileSync(filePath,"This file was written using writeFileSync().\nSynchronous operation block execution");
-// Append to a file
-fs.appendFile(filePath,"New text added using fs.appendFile.",
-    function(error){
-        if(error){
-            console.log("Append error: ",error.message);
-            return;
-        }
+//Append to a file
+fs.appendFile(filePath,"New text added using fs.appendFile.",function(error){
+    if(error){
+        console.log("Append error:",error.message);
+        return;
     }
-)
+    console.log("File content appended");
+});
 
 console.log("File written synchronously.");
 
 const content = fs.readFileSync(filePath,"utf-8");
 
-console.log("File Read Synchronously.");
-
-console.log(content);
+console.log("File read synchronously.");
+console.log("File content:\n",content);
