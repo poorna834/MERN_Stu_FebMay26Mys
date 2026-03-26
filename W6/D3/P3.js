a3b9ea95772723caa6fe6120f812c23d6203cffb
poorna834 <poornashree.lm@gmail.com>
2026-03-26 09:31:46 +0530

    Reading and writing files asynchronously with callbacks

--- W6/D3/P3.js
+++ W6/D3/P3.js
@@ -1,32 +1,34 @@
-// Reading And Writing files asynchronously with callbacks.
-
+//Reading and writing files asynchronously with callbacks
 const fs = require("fs");
 const path = require("path");
 
 const filePath = path.join(__dirname,"async-note.txt");
 
-// To write to a file
-fs.writeFile(
-    filePath,
-    "This is written asynchronously using wwriteFile().",
-    function(writeError){
-        if(writeError){
-            console.log("Write erroe: ",writeError.message);
-            return;
-        }
-        console.log("File written Asynchronously.");
-
+// Append file
+fs.appendFile(filePath,"This is written asynchronously using writeFile().",function(writeError){
+    if(writeError){
+        console.log("Write error: ",writeError.message);
+        return;
     }
-);
+    console.log("File content appended.");
+});
 
-// read from a file
-fs.readFile(filePath,"utf-8",
-    function(readError,content){
-        if(readError){
-            console.log("Read error: ",readError.message);
-            return;
-        }
-        console.log(content);
+//To write to a file
+fs.writeFile(filePath,"This is written asynchronously using writeFile().",function(writeError){
+    if(writeError){
+        console.log("Write error: ",writeError.message);
+        return;
     }
-);
+    console.log("File written asynchronously.");
+});
+
+//Read from a file
+const content = fs.readFile(filePath,"utf-8",function(readError,content){
+    if(readError){
+    console.log("Read error:",readError.message);
+    return;
+    }
+    console.log(content);
+});
+
 console.log("Script continues while file operations are in progress.");
