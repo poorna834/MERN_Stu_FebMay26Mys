afb8fd1651dc256ecbb11f0632ae6b6028fdf6e9
poorna834 <poornashree.lm@gmail.com>
2026-03-30 14:51:27 +0530

    Projects

--- W6/Portfolio_Project_V3.0/Portfolio Project/js/features/ProjectResultCountDisplay.js
+++
@@ -1,6 +0,0 @@
-const resultCount = document.getElementById('result-count');
-
-// update count function
-function updateResultCount(filteredProjects) {
-  resultCount.textContent = `${filteredProjects.length} project(s) found`;
-}
