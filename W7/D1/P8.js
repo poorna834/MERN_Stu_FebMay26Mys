81a259807afe53fe397f8d5382cfbd5a1f9fca3c
poorna834 <poornashree.lm@gmail.com>
2026-03-31 14:36:17 +0530

    Route Parameters and Query Parameters

--- W7/D1/P8.js
+++ W7/D1/P8.js
@@ -1,1 +1,17 @@
-// 
+// Route Parameters and Query Parameters
+// Route Parameters:Capture dynamic values from the path
+// Query Parameters:provide optional values
+
+const express = require('express');    
+const app = express();
+
+app.get("/products/:id",function(req,res){
+    res.json({
+        routeParams:req.params.id,
+        queryParams:req.query
+    });
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
