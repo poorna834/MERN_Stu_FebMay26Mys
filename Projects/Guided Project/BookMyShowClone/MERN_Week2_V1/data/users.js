e0d51de3f9cc8a32b4592455d7ee81bc08a9cae7
poorna834 <poornashree.lm@gmail.com>
2026-04-08 10:07:57 +0530

    User's Details Of The Application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,21 @@
+// User's Details Of The Application.
+
+const users = [
+    {
+        id:1,
+        name:"Poorna",
+        email:"u@e.com",
+        password:"poorna123",
+        role:"user"
+    },
+
+    {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    },
+];
+
+module.exports = users;
