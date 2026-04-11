283b78b88a5c287bad419653b50f69e2adcdbee4
poorna834 <poornashree.lm@gmail.com>
2026-04-11 12:52:18 +0530

    Connecting to MongoDb to NodeJS using Mongoose and defining Schema & models

--- W8/D1/P1.js
+++ W8/D1/P1.js
@@ -1,8 +1,8 @@
-// Connecting to mongoDB to NodeJS using mongoose and defining Schema & models
+// Connecting to MongoDb to NodeJS using Mongoose and defining Schema & models
 const mongoose = require("mongoose");
 
 async function main(){
-    try {
+    try{
         await mongoose.connect("mongodb://localhost:27017/abcmern");
         console.log("MongoDB connected successfully");
 
@@ -14,13 +14,13 @@ async function main(){
 
         const User = mongoose.model("User",userSchema);
 
-        console.log("Mongoose schema & model created successfully");
+        console.log("mongoose schema & model created succesfully");
         console.log("Model name:",User.modelName);
 
         await mongoose.connection.close();
         console.log("connection closed");
     }
-    catch(error) {
+    catch(error){
         console.log("Failed to connect to DB",error.message);
     }
 }
