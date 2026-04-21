ddea83a5442ee9a6b02fa172e94bfbac2518d815
poorna834 <poornashree.lm@gmail.com>
2026-04-21 15:01:45 +0530

    Added Auth Routes

--- Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const authRoutes = require("./routesauth.routes");
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth",authRoutes);
 
+// Base URL
 app.get('/',(req,res) => {
     res.status(200).json({ 
     success:true,
