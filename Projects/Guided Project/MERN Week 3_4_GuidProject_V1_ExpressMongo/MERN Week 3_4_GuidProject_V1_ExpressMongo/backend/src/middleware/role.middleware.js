b3476e3f2207651d6b8d993d27fc216cbe933ae4
poorna834 <poornashree.lm@gmail.com>
2026-04-22 09:58:10 +0530

    Created Role middleware for RBAC

---
+++ Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/middleware/role.middleware.js
@@ -0,0 +1,12 @@
+// Role Middleware: RBC
+exports.authorize = (...roles)=>{
+    return(req,res,next)=>{
+        if(!req.user || !roles.includes(req.user.role)){
+            return res.status(403).json({
+                success:false,
+                message:"Access Denied: Insufficient Permission",
+            });
+        }
+        next();
+    };
+};
