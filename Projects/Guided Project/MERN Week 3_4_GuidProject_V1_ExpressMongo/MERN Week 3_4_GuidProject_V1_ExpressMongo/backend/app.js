bb5f104a296b47ded98ae2ac54a46cbecbcf7c82
poorna834 <poornashree.lm@gmail.com>
2026-04-22 12:19:08 +0530

    Imported movie and booking routes and added to app.use

--- Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const authRoutes = require("./src/routes/auth.routes");
+const movieRoutes = require("./src/routes/movie.routes");
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/auth",authRoutes);
 
-//Base URL
-app.get("/",(req,res)=>{
+// Route Layers
+app.use("/api/auth", authRoutes);
+app.use("/api/movies", movieRoutes);
+
+// Base URL
+app.get("/", (req, res) => {
     res.status(200).json({
-        success:true,
+        success: true,
         message: "Movie booking API is running...",
     });
 });
-
 
 module.exports = app;
