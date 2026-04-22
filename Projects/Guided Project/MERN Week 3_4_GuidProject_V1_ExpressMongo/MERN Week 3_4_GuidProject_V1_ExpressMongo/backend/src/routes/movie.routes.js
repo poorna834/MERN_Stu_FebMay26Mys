2662aa28f7bbccd46bc1f780e19b71bdfc80ab72
poorna834 <poornashree.lm@gmail.com>
2026-04-22 12:19:43 +0530

    Create public and admin route with auth & role middleware

--- Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/movie.routes.js
+++ Projects/Guided Project/MERN Week 3_4_GuidProject_V1_ExpressMongo/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/routes/movie.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = require.Router();
+const router = express.Router();
 
 const {protect} = require("../middleware/auth.middleware");
 const {authorize} = require("../middleware/role.middleware");
