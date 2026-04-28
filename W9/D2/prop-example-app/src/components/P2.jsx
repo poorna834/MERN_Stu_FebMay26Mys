09f8dd1a4b8b0b9ad516872ae7b1fb2079fb359d
poorna834 <poornashree.lm@gmail.com>
2026-04-28 15:41:08 +0530

    Prop Destructuring

---
+++ W9/D2/prop-example-app/src/components/P2.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function UserProfile({ username, skill }) {
+    return (
+        <div>
+            <p>User: {username}</p>
+            <p>Skill: {skill}</p>
+        </div>
+    );
+}
+
+// Parent Component
+export function PropDestructuring() {
+    return (
+        <>
+            <h2>Prop Destructuring</h2>
+            <UserProfile username="Poorna" skill="React" />
+        </>
+    );
+}
