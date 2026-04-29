7142a0f9974434a04b3d871b3b0ac88f9c951461
poorna834 <poornashree.lm@gmail.com>
2026-04-29 15:15:23 +0530

    Higher order component(HOC) example Added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -9,7 +9,8 @@ import { ObjectsAndArrayProps } from "./components/P7";
 import { PropsImmutability } from "./components/P8";
 import { PropsState } from "./components/P9";
 import { FunctionProps } from "./components/P10";
-import { PropTypesDemo } from "./components/P11";
+import { PropTypesDemo } from "./components/P11"; 
+import { HOCExample } from "./components/P12"
 
 function App() {
   return (
@@ -24,7 +25,8 @@ function App() {
       {/* <PropsImmutability /> */}
       {/* <PropsState/> */}
       {/* <FunctionProps /> */}
-      <PropTypesDemo />
+      {/* <PropTypesDemo /> */}
+      <HOCExample/>
     </>
   )
 }
