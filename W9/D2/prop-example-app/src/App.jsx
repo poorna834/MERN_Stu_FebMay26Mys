81872d8f5769649ca6bb3fd0c37fc7586e719fc8
poorna834 <poornashree.lm@gmail.com>
2026-04-29 13:53:05 +0530

    Passing objects and arrays as props Added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -5,6 +5,8 @@ import { PropsChildren } from './components/P3'
 import { PropDrillingDemo } from './components/P4'
 import { SharedStateParent } from './components/P5'
 import { CompositionDemo } from './components/P6'
+import { ObjectsAndArrayProps } from './components/P7'
+// import { PropsImmutability } from './components/P8'
 import './App.css'
 
 function App() {
@@ -17,7 +19,9 @@ function App() {
       {/* <PropsChildren/> */}
       {/* <PropDrillingDemo/> */}
       {/* <SharedStateParent/> */}
-      <CompositionDemo/>
+      {/* <CompositionDemo/> */}
+      <ObjectsAndArrayProps/>
+      {/* <PropsImmutability/> */}
     </>
   )
 }
