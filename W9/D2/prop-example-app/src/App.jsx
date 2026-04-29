9c0e8fff9523fbcf4836d147215ebf1f61095d08
poorna834 <poornashree.lm@gmail.com>
2026-04-29 11:44:27 +0530

    State lifting added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -3,6 +3,7 @@ import { PropBasics } from './components/P1'
 import { PropDestructuring } from './components/P2'
 import { PropsChildren } from './components/P3'
 import { PropDrillingDemo } from './components/P4'
+import { SharedStateParent } from './components/P5'
 import './App.css'
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       {/* <PropBasics/> */}
       {/* <PropDestructuring/> */}
       {/* <PropsChildren/> */}
-      <PropDrillingDemo/>
+      {/* <PropDrillingDemo/> */}
+      <SharedStateParent/>
     </>
   )
 }
