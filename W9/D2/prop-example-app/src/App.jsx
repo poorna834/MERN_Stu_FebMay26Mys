5f0f10aad33726e0ac75cd3b2817e18769114c8c
poorna834 <poornashree.lm@gmail.com>
2026-04-29 10:24:05 +0530

    Props Drilling Added

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { PropBasics } from './components/P1'
 import { PropDestructuring } from './components/P2'
 import { PropsChildren } from './components/P3'
+import { PropDrillingDemo } from './components/P4'
 import './App.css'
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
     <>
       {/* <PropBasics/> */}
       {/* <PropDestructuring/> */}
-      <PropsChildren/>
+      {/* <PropsChildren/> */}
+      <PropDrillingDemo/>
     </>
   )
 }
