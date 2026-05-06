75d9f9270ff7310e968698dd4f121bce3e443edf
poorna834 <poornashree.lm@gmail.com>
2026-05-06 12:00:52 +0530

    Custom hooks Added

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import { UseContextIntro } from './components/P1'
 import './App.css'
-
+import { UseContextIntro } from './components/P1'
+import { CustomHooksIntro } from './components/P2'
 function App() {
-  
+
   return (
-  <>
-      <UseContextIntro/>
-  </>
+    <>
+      {/* <UseContextIntro /> */}
+      <CustomHooksIntro />
+    </>
   )
 }
 
