ae71fa5c597d8bfd584dd47ce34e968c3c735b64
poorna834 <poornashree.lm@gmail.com>
2026-05-06 14:02:50 +0530

    useRef hook added

--- W10/D2/my-app/src/App.jsx
+++ W10/D2/my-app/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import { UseContextIntro } from './components/P1'
-import { CustomHooksIntro } from './components/P2'
+import { UseContextIntro } from "./components/P1";
+import { CustomHooksIntro} from "./components/P2";
+import { UserRefIntro} from "./components/P3";
+// import { UseReducerIntro } from "./components/P4";
 function App() {
-
   return (
     <>
-      {/* <UseContextIntro /> */}
-      <CustomHooksIntro />
+      {/* <UseContextIntro />        */}
+      {/* <CustomHooksIntro /> */}
+      <UserRefIntro />
+      {/* <UseReducerIntro /> */}
     </>
   )
 }
-
 export default App
