6bd0b6dc7622128fc906566dc245b5241d3b727f
poorna834 <poornashree.lm@gmail.com>
2026-05-25 12:43:26 +0530

    Error boundary & file upload progress

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -1,18 +1,21 @@
+import './App.css'
 import { ManagingApiState } from "./components/P1";
 import { UseEffectLifecycle } from "./components/P2";
-import { AxiosLifeCycle } from "./components/P3";
+import { AxiosLifecycle } from "./components/P3";
 import { CRUDOperation } from "./components/P4";
 import { PaginationCaching } from "./components/P5";
-import './App.css'
+import { UploadErrorBoundary } from "./components/P6";
 
 function App() {
+ 
   return (
     <>
-     {/* <ManagingApiState /> */}
-     {/* < UseEffectLifecycle /> */}
-     {/* < AxiosLifeCycle /> */}
-     {/* < CRUDOperation /> */}
-     < PaginationCaching />
+        {/* <ManagingApiState /> */}
+        {/* <UseEffectLifecycle /> */}
+        {/* <AxiosLifecycle /> */}
+        {/* <CRUDOperation /> */}
+        {/* <PaginationCaching/> */}
+        <UploadErrorBoundary/>
     </>
   )
 }
