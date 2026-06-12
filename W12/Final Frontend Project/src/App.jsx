3d12a8dc6001e6b7ccf700c80daf1c9e33204fd4
poorna834 <poornashree.lm@gmail.com>
2026-06-12 15:18:15 +0530

    Conditional Rendering

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,24 +1,32 @@
 import './App.css';
-import HomePage from "./pages/HomePage";
-import CitySelector from "./components/CitySelector";
-import MovieSearch from "./components/MovieSearch";
-import BookButtton from "./components/BookButton";
-import GenreFilter from "./components/GenreFilter";
-import LoginForm from "./components/LoginForm";
-import SearchShortcut from "./components/SearchShortcut";
+// import HomePage from "./pages/HomePage";
+// import CitySelector from "./components/CitySelector";
+// import MovieSearch from "./components/MovieSearch";
+// import BookButtton from "./components/BookButton";
+// import GenreFilter from "./components/GenreFilter";
+// import LoginForm from "./components/LoginForm";
+// import SearchShortcut from "./components/SearchShortcut";
+import AuthStatus from "./components/AuthStatus";
+import MovieLoader from "./components/MovieLoader";
+import MovieResults from "./components/MovieResults";
 
 function App() {
   return (
     <>
-      <CitySelector/>
+      {/* <CitySelector/> */}
       {/* Controlled Component */}
-      <MovieSearch/>
+      {/* <MovieSearch/>
       <BookButtton/>
       <GenreFilter/>
       <LoginForm/>
-      <SearchShortcut/>
+      <SearchShortcut/> */}
+      <AuthStatus/>
+      <MovieLoader/>
+      <MovieResults/>
+
     </>
   );
 }
 
 export default App;
+
