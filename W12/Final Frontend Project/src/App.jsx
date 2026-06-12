a0d228a6a5b9af43e9830d152fd6b8f797eb7e2a
poorna834 <poornashree.lm@gmail.com>
2026-06-12 12:06:26 +0530

    Usage Of Props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
-import HomePage from "./pages/HomePage"
-function App() {
-  
-  return (
-    // Component composition
-      <HomePage/>
+import './App.css';
+// import HomePage from "./pages/HomePage";
+import MovieCard from "./components/MovieCard";
 
+function App() {
+  return (
+    <>
+      <MovieCard
+        title="Inception"
+        genre="Sci-Fi"
+        rating={4.8}
+        duration="2h 28m"
+        poster="https://picsum.photos/200/300?1"
+      />
+
+      <MovieCard
+        title="Avatar"
+        genre="Sci-Fi"
+        rating={4.1}
+        duration="3h 28m"
+        poster="https://picsum.photos/200/300?2"
+      />
+
+      <MovieCard
+        title="Fight Club"
+        genre="Sci-fi"
+        rating={4.9}
+        duration="2h 19m"
+        poster="https://picsum.photos/200/300?3"
+      />
+    </>
   );
 }
 
-export default App
+export default App;
